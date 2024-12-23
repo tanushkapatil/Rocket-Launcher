aef3f661faaf000decacd4dfcd9fafb56b2d727a
Tanushka Patil <94859584+tanushkapatil@users.noreply.github.com>
2024-12-23 23:25:58 +0530

    Update Movement.cs

--- Movement.cs
+++ Movement.cs
@@ -5,7 +5,10 @@ using UnityEngine.InputSystem;
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotation ;
     [SerializeField] float thrustStrength = 1000f;
+    [SerializeField] float rotationStrength = 100f;
+    
     Rigidbody rb ;
 
     private void Start() {
@@ -14,12 +17,38 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
     private void OnEnable() {
         thrust.Enable();
+        rotation.Enable();  
     }
 
-    private void FixedUpdate() {
-        if(thrust.IsPressed()){
+    private void FixedUpdate()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessThrust()
+    {
+        if (thrust.IsPressed())
+        {
             rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
         }
     }
-    
+
+    private void ProcessRotation() {
+        float rotationinput = rotation.ReadValue<float>() ; 
+        if(rotationinput > 0) {
+            ApplyRotation(-rotationStrength);
+        }
+        else if(rotationinput < 0)
+        {
+            ApplyRotation(rotationStrength);
+        }
+    }
+
+    private void ApplyRotation(float rotationframe)
+    {
+        rb.freezeRotation = true;
+        transform.Rotate(Vector3.forward * rotationframe * Time.fixedDeltaTime);
+        rb.freezeRotation = false;
+    }
 }
